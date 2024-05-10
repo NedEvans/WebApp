bdfb787916714a2c00bacd2f324a467383b291df
NedEvans <hrh.neilevans@gmail.com>
2024-05-11 06:37:49 +0800

    First Build

--- WebApp/obj/Debug/net7.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net7.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66191266a8e5c697d7bdba7b9c82c18bfd429de6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
